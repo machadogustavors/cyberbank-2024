8d2c1be17fbba56583f55cd47187d34a43957b95
machadogustavors <machado.gustavo2905@gmail.com>
2024-11-13 20:47:33 -0300

    post and delete mapping

--- api/src/main/java/br/com/poo/poobank/controller/UsuarioController.java
+++ api/src/main/java/br/com/poo/poobank/controller/UsuarioController.java
@@ -3,12 +3,17 @@ package br.com.poo.poobank.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.poo.poobank.domain.Usuario;
 import br.com.poo.poobank.repository.UsuarioRepository;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/usuario")
@@ -22,4 +27,24 @@ public class UsuarioController {
         //return repository.buscarSemSenha();
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Usuario carregarUser(@PathVariable Integer id){
+        return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public String save(@RequestBody Usuario usuario){
+        repository.save(usuario);
+
+        return "OK";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Integer id){
+        repository.deleteById(id);
+
+        return "OK";
+    }
+
 }
